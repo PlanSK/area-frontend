d08b97918e1ff9e48934c9cc8fd36c52dc270526
PlanSK <skapkanshchikov@gmail.com>
2023-09-22 22:19:30 +0300

    Refactoring humanize function.

--- src/services.ts
+++ src/services.ts
@@ -1,22 +1,17 @@
 export function humanizeNumber(value: number, separator: string = " ") {
-    let humanizeNumberString: string = "";
-    let strNumber: string = String(value);
-    let splittedStr: Array<string> = strNumber.split('.');
-    let firstPart: string = splittedStr[0];
-    let secondPart: string = "";
-    splittedStr.length === 1 ? secondPart = '00' : secondPart = splittedStr[1];
+    let humanizeNumberString = "";
+    let [firstPart, secondPart] = String(value).split('.');
+    !secondPart && (secondPart = '00')
 
     if (firstPart.length > 3) {
         let remainder = firstPart.length % 3;
-        let leftPart: string;
-        remainder > 0 ? leftPart = firstPart.substring(0, remainder) + separator : leftPart = ""
         let countRightParts = Math.floor(firstPart.length / 3);
-
-        humanizeNumberString = leftPart;
+        remainder > 0 && (humanizeNumberString = firstPart.substring(0, remainder) + separator)
+        
         for (let i = remainder; i < firstPart.length; i = i + 3) {
             countRightParts--;
             humanizeNumberString += firstPart.substring(i, i + 3);
-            if (countRightParts > 0) humanizeNumberString += separator;
+            countRightParts > 0 && (humanizeNumberString += separator)
         }
     } else humanizeNumberString = firstPart;
 
